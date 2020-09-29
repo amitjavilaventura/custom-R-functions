e3481bf11198b52ff1ae6fabbada0cbfac67cea8
Adrià Mitjavila Ventura <adria.mitjavila94@gmail.com>
2020-09-29 11:43:15 +0200

    Update volcanoPlot2

--- volcanoplot2.R
+++ volcanoplot2.R
@@ -2,78 +2,153 @@
 ###     VOLCANO PLOT 2     ###
 ##############################
 
-# ggplot2-based fucntion made by Dani. I modified it so if I put all together in the same panel, it is easier to modify.
-# Added:
-#   -Change colors of points
-#   -Change the axis label names and sizes
-#   -Change the title size
-#   -Change the legend position
-#   -Changed color and position of the number of up/down DEGs
+#' @title volcanoPlot2
+#' @author amitjavilaventura & dfernandezperez
+#' 
+#' @usage colcanoPlot2(df, xlim = c(-10,10), ylim = c(0,30), pval = 0.05, log2FC = 1.5, main = NULL, mainSize = 9, sub = NULL, subSize = 8, labelSize = 7, labelColor = c("darkgreen", "red"), labelPos = 0, xlab = "log2(FC)", ylab = "-log10(pval)", axisLabelSize = 7, axisTextSize = 7, pointColor = c("darkgreen", "gray", "red"), legendTitle = FALSE, legendPos = "bottom", degsLabel=F, degsLabelNum=5, ggrastr = F)
+#' 
+#' Function that draws a volcano plot with DEGs. 
+#' As input, it takes the output of DESeq2 and adding a DEG information column (Downregulated, Upregulated, NotDE). Columns: Geneid, baseMean, log2FoldChange, lfcSE, pvalue, padj, DEG.
+#' It also counts the number of up and downregulated genes and writes them in the plot.
+#' The points that are outside of the limits of the graph are drawn as triangles at the borders. 
+#'
+#' @param df Dataframe. Output from DESeq2 with an extra DEG column. Columns: Geneid, baseMean, log2FoldChange, lfcSE, pvalue, padj, DEG.
+#' @param xlim Numerical of length 2. The limits of the x axis where the log2FC is plotted. Default: c(-10,10).
+#' @param ylim Numerical of length 2. The limits of the y axis where the -log10(adj_pval) is plotted.  Default: c(0,30).
+#' @param pval Numerical. p-value threshold used to call de DEGs. It is used to draw a dashed line in this value. Default: 0.05.
+#' @param log2FC Numerical. log2FC threshold (in absolute value) used to call the DEGs. It is used to draw two lines in this value (positive and negative). Default: 1.5.
+#' @param main Character. The title of the volcano plot. Default: NULL. 
+#' @param mainSize Numerical. The size of the title. Default: 9.
+#' @param sub Character. The subtitle of the volcano plot. Default: NULL
+#' @param subSize. Numerical. The size of the subtitle. Default: 8.
+#' @param xlab Character. label of the X axis. Default: "log2(FC)".
+#' @param ylab Character. Label of the Y axis. Default: "-log10(pval)".
+#' @param labelSize Numerical. The size of the numbers of up and downregulated genes. Default: 7.
+#' @param labelColor Character of length 2. Colors of the numbers of downregulated and upregulated genes. Default: c("darkgreen", "red").
+#' @param labelPos Numerical. Position of the numbers of up and downregulated genes along the y axis. Default: 0.
+#' @param axisLabelSize Numerical. Size of the axis labels. Default: 7.
+#' @param axisText_Size Numerical. Size of the text in the axis. Default: 7.
+#' @param pointColor Character of length 3. Colors of the downregulated, notDE and upregulated points. Default: c("darkgreen", "gray", "red")
+#' @param legendTitle Logical. If FALSE, the title of the legend is not plotted. Default: FALSE.
+#' @param legendPos Character. Position of the legend. One of: "bottom", "top", "right", "left", "none". Default: "bottom".
+#' @param degsLabel Logical. If TRUE, the most differentially expressed genes are labelled. Default: FALSE.
+#' @param degsLabelNum Numerical. Number of most expressed genes to label in the plot. Default: 5.
+#' @param ggrastr Logical. If TRUE, the points of the volcano plot are drawn with geom_points_rast instead of geom_point. Default: FALSE.
 
-volcanoPlot2 <- function(df, xlim = c(-10,10), ylim = c(0,30), main = NULL, title_size = 9, 
-                         labelSize = 7, labelColor = c("green", "darkred"), labelPos = 0,
-                         pval = 0.05, log2FC = 1.5, xlab = "log2(FC)", ylab = "-log10(pval)", axis_label_size = 7, axis_text_size = 7, 
-                         point.color = c("darkgreen", "gray", "red"), legend_title = FALSE, legend_pos = "bottom", 
-                         degsLabelNum = 5, degsLabel = F) {
+
+volcanoPlot2(org_apc_vs_nownt, log2FC = 1)
+
+volcanoPlot2 <- function(df, xlim = c(-10,10), ylim = c(0,30),
+                         pval = 0.05, log2FC = 1.5,
+                         main = NULL, mainSize = 9, sub = NULL, subSize = 8,
+                         labelSize = 7, labelColor = c("darkgreen", "red"), labelPos = 0,
+                         xlab = "log2(FC)", ylab = "-log10(pval)", axisLabelSize = 7, axisTextSize = 7, 
+                         pointColor = c("darkgreen", "gray", "red"), legendTitle = FALSE, legendPos = "bottom",
+                         degsLabel=F, degsLabelNum=5,
+                         ggrastr = F) {
   
-  #load package ggplot2
+  #load packages
   require(ggplot2)
   require(dplyr)
   
-  #generate the graph with ggplot(), stablish the foldchange and pvalue to colour the DEGs and stablish the point format.
-  p <-  ggplot(data = df, aes(x=log2FoldChange, y=-log10(padj), colour=DEG) ) +
-    geom_point(size=1.5) +
+  # Mutate the dataframe to include the shape of the points.
+  df <- df %>%
     
-    #annotate the number of up and downregulated DEGs
+    # Shape of al the points
+    dplyr::mutate(shape = "circle") %>%
+    
+    # Change the shape of the point to triangle if the -log10(padj) is greater than the ylim variable
+    dplyr::mutate(shape = ifelse(test = -log10(padj) > ylim[2], yes = "triangle", no = shape)) %>%
+    
+    # Change the shape of the points to triangle if the log2FoldChange is greater or lower than the xlim
+    dplyr::mutate(shape = ifelse(test = log2FoldChange > xlim[2], yes = "triangle", no = shape)) %>%
+    dplyr::mutate(shape = ifelse(test = log2FoldChange < xlim[1], yes = "triangle", no = shape)) %>%
+    
+    # Change the padj to the value that plots the points at the top of the graph.
+    dplyr::mutate(padj  = ifelse(test = -log10(padj) > ylim[2], yes = 10^-ylim[2], no = padj)) %>%
+    
+    # Change the log2FoldChange of the points with log2FoldChange than the limits, so they will be plotted in the limits of the graph
+    dplyr::mutate(log2FoldChange = ifelse(test = log2FoldChange > xlim[2], yes = xlim[2], no = log2FoldChange)) %>%
+    dplyr::mutate(log2FoldChange = ifelse(test = log2FoldChange < xlim[1], yes = xlim[1], no = log2FoldChange))
+    
+    
+  # Start plot with ggrastr points. This is useful when using workflowr.
+  if(ggrastr){
+    
+    # Load ggrastr
+    require(ggrastr)
+    
+    p <- ggplot(data = na.omit(df), aes(x=log2FoldChange, y=-log10(padj), colour=DEG, shape=shape)) +
+    
+      # Draw points with geom_point_rast from ggrastr, because it will draw points as images, making the graph lighter.
+      geom_point_rast(alpha=0.7, size=1.7, raster.height = 5.15, raster.width = 6, raster.dpi = 400)
+  }
+  
+  # Start plot normally
+  else{
+    p <- ggplot(data = na.omit(df), aes(x=log2FoldChange, y=-log10(padj), colour=DEG, shape=shape)) + 
+      
+      geom_point(alpha=0.7, size=1.7)
+  }
+  
+  # Annotate the number of up and downregulated DEGs
+  p <- p +
     annotate("text", label = sum(df$DEG == "Upregulated"), color = labelColor[2], y = labelPos, x = xlim[2], 
              vjust=0.5,hjust="inward", size = labelSize) +
     annotate("text", label = sum(df$DEG == "Downregulated"), color = labelColor[1], y = labelPos, x = xlim[1],
              vjust=0.5,hjust="inward", size = labelSize) + 
     
-    #stablish a predefined theme
+    # Stablish a predefined theme
     theme_classic() +
     
-    #write and format the graph title, can be nothing. 
-    ggtitle(main) +
-    theme(plot.title = element_text(face="bold", hjust = .5, size = title_size)) +
+    # Write and format the graph title, can be nothing. 
+    ggtitle(label = main, subtitle = sub) +
+    theme(plot.title = element_text(face="bold", hjust = .5, size = mainSize),
+          plot.subtitle = element_text(hjust = .5, size = subSize)) +
     
-    #stablish the x and y axes ranges. 
-    xlim(xlim) + ylim(ylim) + 
+    # Stablish the x and y axes ranges. 
+    coord_cartesian(xlim = xlim, ylim = ylim) +
     
-    #put an horizontal line in the -log10(pval) value and two vertival lines in the -logFC and logFC values. 
+    # Put an horizontal line in the -log10(pval) value and two vertival lines in the -logFC and logFC values. 
     geom_hline(yintercept = -log10(pval), linetype = 2) +
     geom_vline(xintercept = c(-log2FC, log2FC), linetype = 2) +
     
-    #format the axis names and sizes
-    xlab(xlab) + ylab(ylab) + theme(axis.title = element_text(size = axis_label_size)) +
+    # Format the axis names and sizes
+    xlab(xlab) + ylab(ylab) + theme(axis.title = element_text(size = axisLabelSize)) +
     
-    #format the color of the points
-    scale_colour_manual(values=point.color) +
+    # Format the color of the points
+    scale_colour_manual(values=pointColor) +
     
-    #format the axis values
-    theme(axis.text = element_text(size = axis_text_size)) +
+    # Format the axis values
+    theme(axis.text = element_text(size = axisTextSize)) +
     
-    #decide the position of the legend (default: "bottom")
-    theme(legend.position = legend_pos) 
-
-  #Decide if legend title is writen or not. Default: not writen.  
-  if(legend_title==FALSE){ p <- p + theme(legend.title = element_blank()) }
+    # Decide the position of the legend (default: "bottom")
+    theme(legend.position = legendPos) 
   
-  #Put labels of the genes
+  # Decide if legend title is writen or not. Default: not writen.  
+  if(legendTitle==FALSE){
+    p <- p + theme(legend.title = element_blank())
+  }
+  
+  # Write names of the most upreg genes
   if(degsLabel==TRUE) {
-    #load ggrepel
+    
+    # Load ggrepel
     require(ggrepel)
     
-    # organaize and retrieve most upregulated and most downregulated genes
+    # Organaize and retrieve most upregulated and most downregulated genes
     df$abs_log2fc <- sqrt(df$log2FoldChange**2)
     df  <- df %>% dplyr::arrange(abs_log2fc)
     df2 <- rbind(tail(na.omit(df),degsLabelNum)) %>% as.data.frame()
-    #put labels in the plot
+    df  <- df %>% arrange(padj) 
+    df2 <- rbind(df2, head(df, degsLabelNum))
+    
+    # Put labels in the plot
     p <- p + geom_text_repel(data = df2, mapping = aes(x = log2FoldChange, y = -log10(padj), label = Geneid), size = 5, color = "Black")
     
   }
   
-  #draw the graph.
+  # Draw the graph.
   p
   
 }
