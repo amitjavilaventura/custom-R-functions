5d5d9a993777f43403f039736f05eb043ab9f109
Adrià Mitjavila Ventura <adria.mitjavila94@gmail.com>
2020-11-18 19:26:43 +0100

    fix baranno

--- barAnno.R
+++ barAnno.R
@@ -6,13 +6,13 @@
 #' @author amitjavilaventura
 #'
 #' @usage barAnno(anno_list, names, names_order, protein = NULL, main = NULL, subtitle = NULL, ylab = "Proportion", xlab = NULL,palette = "Set1", legend_position = "right", is_anno = F, is_chip = F, anno_num = 3){
-#' 
+#'
 #' Function for ChIP-seq and ATAC-seq.
 #' It must be used after the function annotatePeak() from the R package ChIPseeker. @seealso \code{\link{annotatePeak}}
 #'
 #' It takes a list of annotation objects that come as output of annotatePeak() and changes the features to "Promoter", "Distal" and "Gene body" (or to "Promoter" and "Distal"). Finally it plots a bargraph with the distribution of all the proportions
 #' As a ggplot2-based function, it allows to add more layers to format the plot.
-#' 
+#'
 #' @param anno_list List of annotation objects that come from annotatePeak().
 #' @param names Charachter vector of the same length as 'anno_list'. Names that will be given to each of the objects in anno_list. Not that will be the names plotted in the bargraph
 #' @param names_order Character vector with the same entries as 'names' with the order wanted to plot the data.
@@ -30,24 +30,24 @@
 
 barAnno  <- function(anno_list, names, names_order,
                      protein = NULL,
-                     main = NULL, 
+                     main = NULL,
                      subtitle = NULL,
                      ylab = NULL, xlab = NULL,
                      palette = "Set1",
-                     legend_position = "right", 
+                     legend_position = "right",
                      is_anno = F, is_chip = F, anno_num = 3,
                      position_fill = T){
-  
+
   # Load packages
   library(dplyr)
   library(purrr)
   library(ggplot2)
   library(ggpubr)
   library(magrittr)
-  
+
   # Create list for annotations from the list of annotatePeak object provided as input
   anno <- list()
-  
+
   # Format promoter annnotation names to take parentheses out for each annotatePeak object in the list
   if(is_anno){
     for(i in 1:length(anno_list)){
@@ -59,39 +59,39 @@ barAnno  <- function(anno_list, names, names_order,
       anno[[i]] <- sub(" \\(.*\\)", "", anno_list[[i]]$annotation)
     }
   }
-  
+
   # Set the names of each annotatePeak object in the list with the vector names
-  anno_df <- set_names(x = anno, nm = names) %>% 
-    
-    # Convert the annotatePeak objects to dataframe 
+  anno_df <- set_names(x = anno, nm = names) %>%
+
+    # Convert the annotatePeak objects to dataframe
     purrr::map(~as.data.frame(x = .x)) %>%
-    
+
     # Write an extra column to each dataframe with the name of the dataframe (provided in names)
-    purrr::imap(~mutate(.data = .x, sample = as.character(.y))) %>% 
-    
+    purrr::imap(~mutate(.data = .x, sample = as.character(.y))) %>%
+
     # Set column names
-    purrr::map(~set_colnames(x = .x, c(c("annotation", "sample")))) %>% 
+    purrr::map(~set_colnames(x = .x, c(c("annotation", "sample")))) %>%
     purrr::map(~dplyr::mutate(.data = .x, annotation = as.character(annotation)))
-    
+
   if(is_chip){
     anno_df <- set_names(x = anno_df, nm = protein) %>%
-        
+
       # Write an extra column to each dataframe with the name of the dataframe (provided in names)
-      purrr::imap(~mutate(.data = .x, protein = as.character(.y))) %>% 
-        
+      purrr::imap(~mutate(.data = .x, protein = as.character(.y))) %>%
+
       # Set column names
       purrr::map(~set_colnames(x = .x, c(c("annotation", "sample", "protein"))))
   }
-    
+
   # Bind dataframes by rows
   anno_df <- anno_df %>% bind_rows() %>%
-    
-    # Mutate the names in order to  
+
+    # Mutate the names in order to
     dplyr::mutate(sample = factor(sample, levels = names_order))
-    
-  # Rewrite annotation as distal or gene body depending on the feature 
-  if(anno_num == 2){  
-    anno_df <- anno_df %>% 
+
+  # Rewrite annotation as distal or gene body depending on the feature
+  if(anno_num == 2){
+    anno_df <- anno_df %>%
       dplyr::mutate(annotation = dplyr::recode(annotation,
                                                "Distal Intergenic" = "Distal",
                                                "Downstream" = "Distal",
@@ -100,8 +100,8 @@ barAnno  <- function(anno_list, names, names_order,
                                                "5' UTR" = "Distal",
                                                "3' UTR" = "Distal"))
   }
-  else if(anno_num == 3){  
-    anno_df <- anno_df %>% 
+  else if(anno_num == 3){
+    anno_df <- anno_df %>%
       dplyr::mutate(annotation = dplyr::recode(annotation,
                                                "Distal Intergenic" = "Distal",
                                                "Downstream" = "Distal",
@@ -110,7 +110,7 @@ barAnno  <- function(anno_list, names, names_order,
                                                "5' UTR" = "Gene body",
                                                "3' UTR" = "Gene body"))
   }
-  
+
   # Create gglpot2-based barplot
   if(position_fill){
     if(is_chip){
@@ -120,8 +120,8 @@ barAnno  <- function(anno_list, names, names_order,
       g <- ggplot(anno_df, aes(sample, fill = annotation)) + geom_bar(position = "fill")
     }
   }
-  
-  else{    
+
+  else{
     if(is_chip){
       g <- ggplot(anno_df, aes(sample, fill = annotation)) + geom_bar() + facet_wrap(~protein)
     }
@@ -131,22 +131,19 @@ barAnno  <- function(anno_list, names, names_order,
   }
     # Write plot title, subtitle and axis labels
   g <- g + ggtitle(main, subtitle = subtitle) + ylab(ylab) + xlab(xlab) +
-    
+
     # Format colors with ggplot2 function scale_fill_brewer
     scale_fill_brewer(palette = palette) +
-    
+
     # Basic formatting
-    theme_pubr() + 
-    
+    theme_pubr() +
+
     theme(axis.text.x = element_text(angle = 20, hjust = 1, vjust = 1),
           legend.title = element_blank(),
           legend.position = legend_position)
-  
+
   # Draw plot
   return(g)
 }
-        
-peak_dist <- tfactors %>% keep(str_detect(names(.), "WTr", negate = TRUE))
-
 
 
