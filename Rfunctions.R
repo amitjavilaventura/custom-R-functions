841401a7a570573d92a94a7de576fd429d4d4add
amitjavilaventura <adria.mitjavila94gmail.com>
2021-07-09 18:17:11 +0200

    update

--- Rfunctions.R
+++ Rfunctions.R
@@ -8,70 +8,70 @@ if(!require(here)){ install.packages("here") }; library(here)
 
 # ----- chromReads ----- #
 # To count and plot the reads mapped in each chromosome
-source(here("R/chromReads.R"))
+source(here::here("R/chromReads.R"))
 
 # ----- pcaplot2 ----- #
 # A function to plot the principal components analysis
-source(here("R/pcaplot2.R"))
+source(here::here("R/pcaplot2.R"))
 
 # ----- volcanoplot2 ----- #
 # A function to draw volcano plots
-source(here("R/volcanoplot2.R"))
+source(here::here("R/volcanoplot2.R"))
 
 # ----- barDEGs ----- #
 # A function to barplots with up and down regulated genes
-source(here("R/barDEGs.R"))
+source(here::here("R/barDEGs.R"))
 
 
 # ----- pieAnno3 ----- #
 # pieAnno -> A function to draw pie charts with the peaks divided by promoter, gene body (UTRs, introns, exons) and distal (distal intergeninc, downstream).
 # filterAnno -> A function that takes the output of Annotate peak and changes the features to promoter or distal.
-source(here("R/pieAnno.R"))
+source(here::here("R/pieAnno.R"))
 
 # ----- barAnno ----- #
 # barAnno -> a function to draw barplots from a list of annotatePeak objects. Can divide peaks in promoter/distal or promoter/gene body/distal
-source(here("R/barAnno.R"))
+source(here::here("R/barAnno.R"))
 
 # ----- vennDiagram2 ----- #
 # vennDiagram2 -> a function to draw venn diagarams from two GenomicRanges objects.
-source(here("R/vennDiagram2.R"))
+source(here::here("R/vennDiagram2.R"))
 
 # ----- read_delim_empty ----- #
 # read_delim_empty -> a read.delim()-based function that can read empty files.
-source(here("R/read_delim_empty.R"))
+source(here::here("R/read_delim_empty.R"))
 
 # ----- def_enhancers ----- #
 # def_enhancers
 # def_enhancers_no_k4me3
-source(here("R/def_enhancers.R"))
+source(here::here("R/def_enhancers.R"))
 
 # ----- DEcompare ----- #
 # DEcompare() -> compares log2FC of two different differential expression datasets (output from dfernandezperez's RNAseq pipeline)
-source(here("R/DEcompare.R"))
+source(here::here("R/DEcompare.R"))
 
 
 # ----- Signals in regions ----- #
 # function(s) that take a bigwig(s) and a bed(s) and compute the signal in the desired regions
-source(here("R/signals_in_regions.R"))
+source(here::here("R/signals_in_regions.R"))
 
 # ----- Upset overlap peaks ----- #
 # upset_overlap_peaks() --> computes the overlaps with different sets of peaks using ChIPpeakAnno::makeVennDiagram() and draws a UpSet plot using UpSetR::upset()
-source(here("R/upset_overlap_peaks.R"))
+source(here::here("R/upset_overlap_peaks.R"))
 
 # ----- ggVennPeaks() ----- #
-source(here("R/ggVennPeaks.R"))
+source(here::here("R/ggVennPeaks.R"))
 
 # ----- FindDeNovoTargets ----- #
-source(here("R/findDeNovoTargets.R"))
+source(here::here("R/findDeNovoTargets.R"))
 
 # ----- Setdiff ----- #
 # Functions from dfernandezperez to find unions and intersections of names
-source(here("R/setDiff.R"))
+source(here::here("R/setDiff.R"))
 
 
 # ----- Ggplot Helpers ----- #
 # Functions to use with ggplot2 and customize the plots easily
-source(here("R/ggplot_helpers.R"))
+source(here::here("R/ggplot_helpers.R"))
 
 # ----- GOenrichment ----- #
 # A function that does enrichGO
